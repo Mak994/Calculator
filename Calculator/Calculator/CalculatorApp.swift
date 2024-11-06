d980ab3e3c681680f2e873ab35d45503ae93a37b
Mak994 <149230513+Mak994@users.noreply.github.com>
2024-11-06 21:12:53 +0530

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mohsin Ali Khaqn on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
